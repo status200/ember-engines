bcaf006333e06fb1eb94bae50cdeebd131a1c5fa
Robert Jackson <rwjblue@twitch.tv>
2016-09-13 20:52:22 -0400

    Simplify engine exports.
    * Avoid relying on initializer to setup `.reopen`'s needed.
    * Use `.extend()` when possible (instead of `Engine.reopen` we can
      `.extend()`)
    * Remove initializer (it is not needed internally).
    * Remove all but one usage of `Ember.__loader.require` (remaining usage
      is for `hasDefaultSerializer`).

--- tests/unit/initializers/engines-test.js
+++ tests/unit/initializers/engines-test.js
@@ -1,5 +1,4 @@
 import Ember from 'ember';
-import EnginesInitializer from '../../../initializers/engines';
 import { module, test } from 'qunit';
 
 let application;
@@ -15,8 +14,6 @@ module('Unit | Initializer | engines', {
 
 // Replace this with your real tests.
 test('it works', function(assert) {
-  EnginesInitializer.initialize(application);
-
   // you would normally confirm the results of the initializer here
   assert.ok(true);
 });
